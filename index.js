7fc200dc6b3df956be5d0deb5f9d5f9ca61a3b88
ggtenev <georgiy.tenev@gmail.com>
2020-03-16 04:37:01 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var socket = require('socket.io')
+
+//APP SETUP
+var app = express()
+var server = app.listen(4000, () => {
+  console.log('Listening')
+})
+
+//Static files
+app.use(express.static('public'))
+
+//Socket setup
+
+var io = socket(server)
+io.on('connection', (socket) => {
+  console.log('made socket connection',socket.id)
+  socket.on('chat' , (data) => {
+    io.sockets.emit('chat', data)
+  })
+  socket.on('typing', data => {
+    socket.broadcast.emit('typing', data)
+  })
+})
